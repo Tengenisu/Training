bed20de4be40b4f786aed8763847f52a9eeae1d4
Tengenisu <154871350+Tengenisu@users.noreply.github.com>
2025-04-02 16:53:11 +0530

    Delete YOLOv8.py

--- YOLOv8.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-
-# Train the model with MPS
-results = model.train(data="/Users/aryanvohra/Documents/Object_detection/Configurations/COCO.yaml", epochs=100, imgsz=640)
